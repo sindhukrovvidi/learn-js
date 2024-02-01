bfe9f91f34fe34effaa41b036c44347fa207d90c
Sindhu Krovvidi <sindhukrovvidi@gmail.com>
2024-02-01 15:29:20 -0500

    Updated quiz changes on class 01/30/24

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,10 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg() {
+    console.log(`Error Message: ${this.errMsg}`)
+}
+
+const obj1 = {
+    errMsg: "Object 1 error message"
+}
+
+logMsg.call(obj1);
